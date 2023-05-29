b2212eb0e26d088d814bff1eef0ac53175c85b6b
Gaurang <rastogigaurang209@gmail.com>
2023-05-29 23:08:37 +0530

    some-what

--- aj_jewellery_python/app.py
+++ aj_jewellery_python/app.py
@@ -1,23 +1,21 @@
+from chatbot  import *
 from flask import Flask
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-mysql = MySQL(app)
+# mysql = MySQL(app)
 
 
-@app.route('/',methods=['GET'])
+@app.route('/chat',methods=['POST'])
 def users():
-    cur = mysql.connection.cursor()
-    cur.execute('''SELECT user, host FROM mysql.user''')
-    rv = cur.fetchall()
-    cur.close()
-    return str(rv)
-
+    return "make function"
 if __name__ == '__main__':
     app.run(debug=True)
 
 
 
+
+print(RuleBot().chat("jewellery"))
 
 # /api call route  ->  latest prices
 # /updateDB route
